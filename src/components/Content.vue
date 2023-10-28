2febd21f1d04b7309dfd040cd82e2b2e2b5ed73c
xuyue <517252939@qq.com>
2023-10-28 23:26:45 +0800

    fix.

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,35 +1,37 @@
 <template>
-  <n-card>
-    <template #header>{{ lesson.title }}</template>
-    {{ lesson.content }}
-  </n-card>
-  <n-card
-    v-if="lesson.nonInteractive"
-    v-for="(item, index) in lesson.code"
-    :key="index"
-  >
-    <n-code language="python" :code="item" show-line-numbers />
-  </n-card>
-  <n-card v-else>
-    <span v-for="(item, index) in lesson.blank" :key="index">
-      <span v-if="item.match(RE)">
-        <n-input
-          placeholder=""
-          :style="{ width: 40 * item.length + 'px' }"
-          v-model:value="inputs[index]"
+  <n-space size="large" vertical>
+    <n-card>
+      <template #header>{{ lesson.title }}</template>
+      {{ lesson.content }}
+    </n-card>
+    <n-card
+      v-if="lesson.nonInteractive"
+      v-for="(item, index) in lesson.code"
+      :key="index"
+    >
+      <n-code language="python" :code="item" show-line-numbers />
+    </n-card>
+    <n-card v-else>
+      <span v-for="(item, index) in lesson.blank" :key="index">
+        <span v-if="item.match(RE)">
+          <n-input
+            placeholder=""
+            :style="{ width: 40 * item.length + 'px' }"
+            v-model:value="inputs[index]"
+          />
+        </span>
+        <br v-else-if="item === '\\n'" />
+        <n-code
+          v-else
+          inline
+          :trim="false"
+          language="python"
+          class="code"
+          :code="item"
         />
       </span>
-      <br v-else-if="item === '\\n'" />
-      <n-code
-        v-else
-        inline
-        :trim="false"
-        language="python"
-        class="code"
-        :code="item"
-      />
-    </span>
-  </n-card>
+    </n-card>
+  </n-space>
 </template>
 <script lang="ts" setup>
 import { inputs, lesson } from "../composables"
